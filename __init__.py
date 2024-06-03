ee129fc2a6f3df6041c14ff7b9e357ad6367ae9e
Vinujancenzo <122785988+Vinujancenzo@users.noreply.github.com>
2024-06-03 14:46:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,25 +77,15 @@ def enregistrer_client():
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
-@app.route('/ulecture')
-def ulecture():
-    if not u_est_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
-        return redirect(url_for('user_authentification'))
 
-    return "<h2>Bravo, vous êtes authentifié</h2>"
-    
 @app.route('/user_authentification', methods=['GET', 'POST'])
-def uauthentification():
+def user_authentification():
     if request.method == 'POST':
         if request.form['username'] == 'user' and request.form['password'] == '12345':
             session['authentifie'] = True
-            return redirect(url_for('ulecture'))
+            return redirect(url_for('lecture'))
         else:
             return render_template('user_auth.html', error=True)
-
-    return render_template('user_auth.html', error=False)
-
 
     return render_template('user_auth.html', error=False)
 
